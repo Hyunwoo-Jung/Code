b474872faa1faf590d4097cb303345c7bd2bce0c
Hyunwoo Jung <nero0115@uos.ac.kr>
2023-07-03 12:37:51 +0900

    first commit

---
+++ calc.py
@@ -0,0 +1,27 @@
+# 거스름돈 금액 구하기
+insert_price = input("insert: ")
+product_price = input("product: ")
+change = int(insert_price)-int(product_price)
+if change<0:
+    print("금액이 부족합니다.")
+else:
+    print("change: ", change)
+
+# 5000원 지폐 매수 구하기
+
+
+# 1000원 지폐 매수 구하기
+
+
+# 500원 동전 개수 구하기
+
+
+# 100원 동전 개수 구하기
+
+
+# 50원 동전 개수 구하기
+
+
+# 10원 동전 개수 구하기
+
+
